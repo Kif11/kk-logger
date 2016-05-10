4cf6dc591f4127e7f4d33f4ad4fcb1931a153f28
Kirill Kovalevskiy <kovalewskiy@gmail.com>
2016-05-10 00:43:17 -0700

    Added bash colors

--- logger.py
+++ logger.py
@@ -4,61 +4,70 @@ import datetime
 
 class Logger(object):
 
-	def __init__ (self, name='', debug=True, log_time=False, log_type=True):
+    def __init__ (self, name='', debug=True, log_time=False, log_type=True):
 
-		self._INFO = '[+]'
-		self._WARNING = '[-]'
-		self._ERROR = '[!]'
-		self._DEBUG = '[D]'
-		self._SEP_CHAR = '-'
-		self.name = name
-		self.log_time = log_time
-		self.log_type = log_type
+        self._INFO = '[+]'
+        self._WARNING = '[-]'
+        self._ERROR = '[!]'
+        self._DEBUG = '[D]'
+        self._SEP_CHAR = '-'
+        self._colors = {
+            'red': '\033[0;31m',
+            'yellow': '\033[0;33m',
+            'end': '\033[0m'
+        }
+        self.name = name
+        self.log_time = log_time
+        self.log_type = log_type
 
-		self.debug_active = debug
+        self.debug_active = debug
 
-	def make_msg(self, msg, msg_type = ''):
-		cur_time = datetime.datetime.now().strftime('%H:%M:%S')
-		if self.log_time:
-			msg = '%s %s' %(cur_time, msg)
-		if self.name:
-			msg = '%s: %s' %(self.name, msg)
-		if self.log_type:
-			msg = '%s %s' %(msg_type, msg)
+    def make_msg(self, msg, msg_type = '', color=''):
 
-		return msg
+        if color in self._colors.keys():
+            color = self._colors[color]
 
-	def log(self, msg):
-		print msg
-		self.flush();
+        cur_time = datetime.datetime.now().strftime('%H:%M:%S')
+        if self.log_time:
+            msg = '%s %s' %(cur_time, msg)
+        if self.name:
+            msg = '%s: %s' %(self.name, msg)
+        if self.log_type:
+            msg = '%s%s %s%s' %(color, msg_type, msg, self._colors['end'])
 
-	def info(self, msg):
-		msg = self.make_msg(msg, self._INFO)
-		self.log(msg)
+        return msg
 
-	def warning(self, msg):
-		msg = self.make_msg(msg, self._WARNING)
-		self.log(msg)
+    def log(self, msg):
+        print msg
+        self.flush();
 
-	def debug(self, msg):
-		if (self.debug_active):
-			msg = self.make_msg(msg, self._DEBUG)
-			self.log(msg)
-		else:
-			pass
+    def info(self, msg):
+        msg = self.make_msg(msg, self._INFO)
+        self.log(msg)
 
-	def error(self, msg):
-		msg = self.make_msg(msg, self._ERROR)
-		self.log(msg)
+    def warning(self, msg):
+        msg = self.make_msg(msg, self._WARNING, color='yellow')
+        self.log(msg)
 
-		# raise NameError('Error reported, aborting render script!')
+    def debug(self, msg):
+        if (self.debug_active):
+            msg = self.make_msg(msg, self._DEBUG)
+            self.log(msg)
+        else:
+            pass
 
-	def line(self):
-		"""
-		Print separation line on 80 chars.
-		"""
-		self.log(self._SEP_CHAR * 80)
+    def error(self, msg):
+        msg = self.make_msg(msg, self._ERROR, color='red')
+        self.log(msg)
 
-	def flush(self):
-		sys.stdout.flush()
-		sys.stderr.flush()
+        # raise NameError('Error reported, aborting render script!')
+
+    def line(self):
+        """
+        Print separation line on 80 chars.
+        """
+        self.log(self._SEP_CHAR * 80)
+
+    def flush(self):
+        sys.stdout.flush()
+        sys.stderr.flush()
